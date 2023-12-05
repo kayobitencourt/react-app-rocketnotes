52a3177d91ee6b16f490093e3b03d0f6567d4def
Kayo Bitencourt <kayobitencourt@gmail.com>
2023-12-05 14:15:21 -0300

    props

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-
+import { ThemeProvider } from 'styled-components'
+import { Details } from './pages/Details'
+import GlobalStyles from './styles/global'
+import theme from './styles/theme'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
+  <React.StrictMode >
+    <ThemeProvider theme={theme}>
+      <GlobalStyles/>
+        <Details />
+    </ThemeProvider>
   </React.StrictMode>,
 )
---
+++ src/pages/Details/index.jsx
@@ -0,0 +1,12 @@
+import { Container } from './styles';
+import { Button } from '../../components/Button'
+export function Details(){
+
+  return(
+  <Container>
+    <h1>Hello World</h1>
+    <span>eae caralho blz meu mano</span>
+    <Button title="Eae arrombado blz" loading/>
+  </Container>
+  )
+}
