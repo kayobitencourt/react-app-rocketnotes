baa706d0ab53a315e5bfb926491ac4807e411085
Kayo Bitencourt <kayobitencourt@gmail.com>
2023-12-06 21:32:04 -0300

    adding profile page

---
+++ src/pages/Profile/index.jsx
@@ -0,0 +1,30 @@
+import { Container, Form, Avatar } from './styles';
+import{ Input } from '../../components/Input'
+import{ Button } from '../../components/Button'
+import { FiArrowLeft, FiUser, FiMail, FiLock, FiCamera } from 'react-icons/fi'
+export function Profile(){
+  return(
+    <Container>
+      <header>
+        <a href="/">
+          <FiArrowLeft/>
+        </a>
+      </header>
+      <Form>
+        <Avatar>
+          <img src="https://github.com/kayobitencourt.png" alt="User picture" />
+          <label htmlFor="avatar">
+            <FiCamera/>
+            <input id="avatar" type="file" />
+          </label>
+        </Avatar>
+
+        <Input placeholder="Nome" type="text" icon={FiUser}/>
+        <Input placeholder="E-Mail" type="text" icon={FiMail}/>
+        <Input placeholder="Senha atual" type="password" icon={FiLock}/>
+        <Input placeholder="Nova senha" type="password" icon={FiLock}/>
+        <Button title="Salvar" />
+      </Form>
+    </Container>
+  );
+}
