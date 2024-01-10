b01f711e3cbe32db4768fd2ed29925b9a02ee1ac
Kayo Bitencourt <kayobitencourt@gmail.com>
2024-01-10 06:53:57 -0300

    Add home configs

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,6 +2,7 @@ import { RiShutDownLine } from 'react-icons/ri'
 import { Container, Profile, Logout } from './styles'
 import{ useAuth } from '../../hooks/auth'
 import { api } from '../../services/api'
+import avatarPlaceHolder from '../../assets/avatar.svg';
 export function Header(){
 
   const { signOut, user } = useAuth();
